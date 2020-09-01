f2b7dca02d2532032bdfa08e36d82d0851a2514d
sk <skbro10@gmail.com>
2020-09-01 15:15:26 +0545

    issue on hover effect players turn

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  var x = "x";
-  var o = "o";
+  var x = 'x';
+  var o = 'o';
   var turns = 0;
 
   var move1 = $('#move1');
@@ -12,41 +12,52 @@ $(document).ready(function () {
   var move7 = $('#move7');
   var move8 = $('#move8');
   var move9 = $('#move9');
+
+  $('#play-board li').mouseover(function () {
+    if ($('#play-board li').hasClass('x-shadow')) {
+      $(this).removeClass('x-shadow');
+      $(this).addClass('o-shadow');
+    } else {
+      $(this).removeClass('o-shadow');
+      $(this).addClass('x-shadow');
+    }
+  });
+  $('#play-board li').mouseout(function () {
+    $(this).removeClass('o-shadow');
+    $(this).removeClass('x-shadow');
+  });
+
   $('#play-board li').on('click', function () {
-    if (move1.hasClass('o') && move2.hasClass('o') && move3.hasClass('o') ||
-      move4.hasClass('o') && move5.hasClass('o') && move6.hasClass('o') ||
-      move7.hasClass('o') && move8.hasClass('o') && move9.hasClass('o') ||
-      move1.hasClass('o') && move4.hasClass('o') && move7.hasClass('o') ||
-      move2.hasClass('o') && move5.hasClass('o') && move8.hasClass('o') ||
-      move3.hasClass('o') && move6.hasClass('o') && move9.hasClass('o') ||
-      move1.hasClass('o') && move5.hasClass('o') && move9.hasClass('o') ||
-      move3.hasClass('o') && move5.hasClass('o') && move7.hasClass('o')
-
+    if (
+      (move1.hasClass('o') && move2.hasClass('o') && move3.hasClass('o')) ||
+      (move4.hasClass('o') && move5.hasClass('o') && move6.hasClass('o')) ||
+      (move7.hasClass('o') && move8.hasClass('o') && move9.hasClass('o')) ||
+      (move1.hasClass('o') && move4.hasClass('o') && move7.hasClass('o')) ||
+      (move2.hasClass('o') && move5.hasClass('o') && move8.hasClass('o')) ||
+      (move3.hasClass('o') && move6.hasClass('o') && move9.hasClass('o')) ||
+      (move1.hasClass('o') && move5.hasClass('o') && move9.hasClass('o')) ||
+      (move3.hasClass('o') && move5.hasClass('o') && move7.hasClass('o'))
     ) {
-
       alert('Winner: P1');
       $('#play-board li').text(' ');
       $('#play-board li').removeClass('disable');
       $('#play-board li').removeClass('o');
       $('#play-board li').removeClass('x');
-
-    } else if (move1.hasClass('x') && move2.hasClass('x') && move3.hasClass('x') ||
-      move4.hasClass('x') && move5.hasClass('x') && move6.hasClass('x') ||
-      move7.hasClass('x') && move8.hasClass('x') && move9.hasClass('x') ||
-      move1.hasClass('x') && move4.hasClass('x') && move7.hasClass('x') ||
-      move2.hasClass('x') && move5.hasClass('x') && move8.hasClass('x') ||
-      move3.hasClass('x') && move6.hasClass('x') && move9.hasClass('x') ||
-      move1.hasClass('x') && move5.hasClass('x') && move9.hasClass('x') ||
-      move3.hasClass('x') && move5.hasClass('x') && move7.hasClass('x')
-
+    } else if (
+      (move1.hasClass('x') && move2.hasClass('x') && move3.hasClass('x')) ||
+      (move4.hasClass('x') && move5.hasClass('x') && move6.hasClass('x')) ||
+      (move7.hasClass('x') && move8.hasClass('x') && move9.hasClass('x')) ||
+      (move1.hasClass('x') && move4.hasClass('x') && move7.hasClass('x')) ||
+      (move2.hasClass('x') && move5.hasClass('x') && move8.hasClass('x')) ||
+      (move3.hasClass('x') && move6.hasClass('x') && move9.hasClass('x')) ||
+      (move1.hasClass('x') && move5.hasClass('x') && move9.hasClass('x')) ||
+      (move3.hasClass('x') && move5.hasClass('x') && move7.hasClass('x'))
     ) {
-
       alert('Winner: P2');
       $('#play-board li').text(' ');
       $('#play-board li').removeClass('disable');
       $('#play-board li').removeClass('o');
       $('#play-board li').removeClass('x');
-
     } else if (turns == 9) {
       alert('Tie Game. Play again');
       $('#play-board li').text(' ');
@@ -60,32 +71,34 @@ $(document).ready(function () {
       turns++;
       $(this).text(o);
       $(this).addClass('disable o');
-      if (move1.hasClass('o') && move2.hasClass('o') && move3.hasClass('o') ||
-        move4.hasClass('o') && move5.hasClass('o') && move6.hasClass('o') ||
-        move7.hasClass('o') && move8.hasClass('o') && move9.hasClass('o') ||
-        move1.hasClass('o') && move4.hasClass('o') && move7.hasClass('o') ||
-        move2.hasClass('o') && move5.hasClass('o') && move8.hasClass('o') ||
-        move3.hasClass('o') && move6.hasClass('o') && move9.hasClass('o') ||
-        move1.hasClass('o') && move5.hasClass('o') && move9.hasClass('o') ||
-        move3.hasClass('o') && move5.hasClass('o') && move7.hasClass('o')
 
+      if (
+        (move1.hasClass('o') && move2.hasClass('o') && move3.hasClass('o')) ||
+        (move4.hasClass('o') && move5.hasClass('o') && move6.hasClass('o')) ||
+        (move7.hasClass('o') && move8.hasClass('o') && move9.hasClass('o')) ||
+        (move1.hasClass('o') && move4.hasClass('o') && move7.hasClass('o')) ||
+        (move2.hasClass('o') && move5.hasClass('o') && move8.hasClass('o')) ||
+        (move3.hasClass('o') && move6.hasClass('o') && move9.hasClass('o')) ||
+        (move1.hasClass('o') && move5.hasClass('o') && move9.hasClass('o')) ||
+        (move3.hasClass('o') && move5.hasClass('o') && move7.hasClass('o'))
       ) {
         alert('Winner: P1');
         turns = 0;
       }
     } else {
       turns++;
+
       $(this).text(x);
       $(this).addClass('disable x');
-      if (move1.hasClass('x') && move2.hasClass('x') && move3.hasClass('x') ||
-        move4.hasClass('x') && move5.hasClass('x') && move6.hasClass('x') ||
-        move7.hasClass('x') && move8.hasClass('x') && move9.hasClass('x') ||
-        move1.hasClass('x') && move4.hasClass('x') && move7.hasClass('x') ||
-        move2.hasClass('x') && move5.hasClass('x') && move8.hasClass('x') ||
-        move3.hasClass('x') && move6.hasClass('x') && move9.hasClass('x') ||
-        move1.hasClass('x') && move5.hasClass('x') && move9.hasClass('x') ||
-        move3.hasClass('x') && move5.hasClass('x') && move7.hasClass('x')
-
+      if (
+        (move1.hasClass('x') && move2.hasClass('x') && move3.hasClass('x')) ||
+        (move4.hasClass('x') && move5.hasClass('x') && move6.hasClass('x')) ||
+        (move7.hasClass('x') && move8.hasClass('x') && move9.hasClass('x')) ||
+        (move1.hasClass('x') && move4.hasClass('x') && move7.hasClass('x')) ||
+        (move2.hasClass('x') && move5.hasClass('x') && move8.hasClass('x')) ||
+        (move3.hasClass('x') && move6.hasClass('x') && move9.hasClass('x')) ||
+        (move1.hasClass('x') && move5.hasClass('x') && move9.hasClass('x')) ||
+        (move3.hasClass('x') && move5.hasClass('x') && move7.hasClass('x'))
       ) {
         alert('Winner: P2');
         turns = 0;
