85f2e756a489ea8b70a110128c0f83b84004f950
sk <skbro10@gmail.com>
2020-09-29 16:09:04 +0545

    Created function to set parent id to append child

--- js/app.js
+++ js/app.js
@@ -1,35 +1,39 @@
+//setting global Varaible
 var winners = [];
-var player1Selections = [];
-var player2Selections = [];
+var playerFirstSelections = [];
+var playerSecondSelections = [];
 var currentPlayer = 0;
 var mark = '';
 var x = prompt('Please enter value over 3'); //input from user
 var boardSize = parseInt(x);
 
-// Function for drawing a Board
-function drawBoard() {
-  var parent; //setting local varaible
-  parent = document.getElementById('play-board');
-  var counter = 1;
-  //while creates a loop with given boardSize for eg = 3 and until we reload the window the board boardSize of the game will be 3 * 3
+function setParentIdToChild(parent) {
   while (parent.hasChildNodes()) {
     parent.removeChild(parent.firstChild);
   }
-  // creating rows and column
+}
+// Function for drawing a Board
+function drawBoard() {
+  var parent;
+  parent = document.getElementById('play-board');
+  var setColumn = 1; //local varaible for setting column
+  //while creates a loop with given boardSize for eg = 3 and until we reload the window the board boardSize of the game will be 3 * 3
+  setParentIdToChild(parent);
+  //drawing the board
   for (size = 0; size < boardSize; size++) {
+    // adding row from the input
     var row = document.createElement('tr');
-    for (r = 0; r < boardSize; r++) {
+    for (rowIndex = 0; rowIndex < boardSize; rowIndex++) {
+      // adding column as per row..here row=column
       var column = document.createElement('td');
-      column.id = counter; // setting col.id = 1;
+      column.id = setColumn; // setting col.id = 1;
       column.addEventListener('click', setHandler);
       row.appendChild(column);
-      counter++;
+      setColumn++;
     }
     parent.appendChild(row);
   }
-  // calling setWinnerComination function
   setWinnerCombination();
-  // adding hover effects by colling function set hover effect
   setHoverEffect();
   console.log(winners);
 }
@@ -39,43 +43,43 @@ function drawBoard() {
 // (3) [4, 5, 6]0: 4 1: 5 2: 6 length: 3
 // (3) [7, 8, 9]0: 7 1: 8 2: 9 length: 3
 function setWinnerCombination() {
-  var valueInRow = 1;
-  var valueInColumn = 1;
-  var valueInDiagonal = 1;
+  var valueForRow = 1;
+  var valueForColumn = 1;
+  var valueForDiagonal = 1;
   for (j = 0; j < boardSize; j++) {
     //for pushing rows into winners array
     var rowIndex = [];
     //i = rowNum
     for (i = 0; i < boardSize; i++) {
-      rowIndex.push(valueInRow);
-      valueInRow++;
+      rowIndex.push(valueForRow);
+      valueForRow++;
     }
     console.log(rowIndex);
     winners.push(rowIndex);
     //for pushing columns into winners array
     var columnIndex = [];
-    var tempValueInColumn = valueInColumn;
+    var tempvalueForColumn = valueForColumn;
     //i = colNum
     for (i = 0; i < boardSize; i++) {
-      columnIndex.push(tempValueInColumn);
-      tempValueInColumn += boardSize;
+      columnIndex.push(tempvalueForColumn);
+      tempvalueForColumn += boardSize;
     }
     winners.push(columnIndex);
-    valueInColumn++;
+    valueForColumn++;
   }
   var diagonalIndexOne = [];
   // l = Digonal Num
   for (l = 0; l < boardSize; l++) {
-    diagonalIndexOne.push(valueInDiagonal);
-    valueInDiagonal += boardSize + 1;
+    diagonalIndexOne.push(valueForDiagonal);
+    valueForDiagonal += boardSize + 1;
   }
   winners.push(diagonalIndexOne);
-  valueInDiagonal = boardSize;
+  valueForDiagonal = boardSize;
   var diagonalIndexTwo = [];
   // l = Digonal Num
   for (l = 0; l < boardSize; l++) {
-    diagonalIndexTwo.push(valueInDiagonal);
-    valueInDiagonal += boardSize - 1;
+    diagonalIndexTwo.push(valueForDiagonal);
+    valueForDiagonal += boardSize - 1;
   }
   winners.push(diagonalIndexTwo);
 }
@@ -84,7 +88,7 @@ function setWinnerCombination() {
 function hasAWinner() {
   var win = false;
   var playerSelections = [];
-  playerSelections = currentPlayer == 0 ? player1Selections : player2Selections;
+  playerSelections = currentPlayer == 0 ? playerFirstSelections : playerSecondSelections;
   if (playerSelections.length >= boardSize) {
     // setting winners from its length
     for (sets = 0; sets < winners.length; sets++) {
@@ -115,8 +119,8 @@ function hasAWinner() {
 //Reset function
 function reset() {
   currentPlayer = 0;
-  player1Selections = [];
-  player2Selections = [];
+  playerFirstSelections = [];
+  playerSecondSelections = [];
 }
 
 // hover function for displaying the turn of player1 and player 2
@@ -147,8 +151,8 @@ var setHandler = function (e) {
   if (currentPlayer == 0) {
     this.innerHTML = 'X';
     mark = 'o';
-    player1Selections.push(parseInt(this.id));
-    player1Selections.sort(function (a, b) {
+    playerFirstSelections.push(parseInt(this.id));
+    playerFirstSelections.sort(function (a, b) {
       return a - b; // sorting in accending order
     });
     this.classList.add('x', 'disable');
@@ -156,8 +160,8 @@ var setHandler = function (e) {
   } else {
     this.innerHTML = 'O';
     mark = 'x';
-    player2Selections.push(parseInt(this.id));
-    player2Selections.sort(function (a, b) {
+    playerSecondSelections.push(parseInt(this.id));
+    playerSecondSelections.sort(function (a, b) {
       return a - b; // sorting in accending order
     });
     this.classList.add('o', 'disable');
@@ -175,7 +179,7 @@ var setHandler = function (e) {
     reset();
     drawBoard();
   } else if (
-    player2Selections.length + player1Selections.length ==
+    playerSecondSelections.length + playerFirstSelections.length ==
     boardSize * boardSize
   ) {
     alert(' Its a tie');
